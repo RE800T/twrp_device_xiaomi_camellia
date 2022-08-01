231dea1342a134ace2e3b83402d9bde06cbfd0c0
RE800T <brutalman122@gmail.com>
2022-08-01 13:15:22 +0300

    Initial TWRP tree

---
+++ device.mk
@@ -0,0 +1,45 @@
+LOCAL_PATH := device/xiaomi/camellia
+
+# Dynamic Partitions
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
+# VNDK
+PRODUCT_TARGET_VNDK_VERSION := 30
+
+# API
+PRODUCT_SHIPPING_API_LEVEL := 30
+
+PRODUCT_PLATFORM := mt6833
+
+# A/B
+ENABLE_VIRTUAL_AB := true
+
+AB_OTA_POSTINSTALL_CONFIG += \
+    RUN_POSTINSTALL_system=true \
+    POSTINSTALL_PATH_system=system/bin/otapreopt_script \
+    FILESYSTEM_TYPE_system=ext4 \
+    POSTINSTALL_OPTIONAL_system=true
+
+# Boot control HAL
+PRODUCT_PACKAGES += \
+    android.hardware.boot@1.1-impl
+
+PRODUCT_PACKAGES_DEBUG += \
+    update_engine_client
+
+PRODUCT_PACKAGES += \
+    otapreopt_script \
+    update_engine \
+    update_verifier \
+    update_engine_sideload
+    
+    
+PRODUCT_PACKAGES += \
+    android.hardware.fastboot@1.0-impl-mock
+
+# health Hal
+PRODUCT_PACKAGES += \
+    android.hardware.health@2.1-impl
+
+TW_OVERRIDE_SYSTEM_PROPS := \
+    "ro.build.product;ro.build.fingerprint;ro.build.version.incremental;ro.product.device=ro.product.system.device;ro.product.model=ro.product.system.model;ro.product.name=ro.product.system.name"
